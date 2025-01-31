4267d06e6d0dc802a0536f316c298ebe37bae931
Patrick Sprague <patricksprague@gmail.com>
2025-01-30 17:50:07 -0800

    added integration tests

---
+++ Tests/IntegrationTest.cs
@@ -0,0 +1,123 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CCI.Selenium.Technical.IntegrationTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.None)]
+    public class IntegrationTest
+    {
+        private ChromeDriver? driver;
+        private readonly By _usernameInputXpath = By.XPath("//input[contains(@id, 'user-name')]");
+        private readonly By _passwordInputXpath = By.XPath("//input[contains(@id, 'password')]");
+        private readonly By _loginButtonXpath = By.XPath("//input[contains(@class, 'submit-button')]");
+        private readonly By _addBackpackCartXpath = By.XPath("//button[contains(@data-test, 'add-to-cart-sauce-labs-backpack')]");
+        private readonly By _shoppingCartXpath = By.XPath("//div[contains(@id, 'shopping_cart_container')]");
+        private readonly By _checkoutXpath = By.XPath("//button[contains(@data-test, 'checkout')]");
+        private readonly By _firstNameXpath = By.XPath("//input[contains(@id, 'first-name')]");
+        private readonly By _lastNameXpath = By.XPath("//input[contains(@id, 'last-name')]");
+        private readonly By _zipCodeXpath = By.XPath("//input[contains(@id, 'postal-code')]");
+        private readonly By _cartContinueXpath = By.XPath("//input[contains(@id, 'continue')]");
+        private readonly By _finishButtonXpath = By.XPath("//button[contains(@data-test, 'finish')]");
+        private readonly By _successMessageXpath = By.XPath("//h2[contains(@data-test, 'complete-header')]");
+        private readonly By _removeBackpackCartXpath = By.XPath("//button[contains(@data-test, 'remove-sauce-labs-backpack')]");
+        private readonly By _continueShoppingXpath = By.XPath("//button[contains(@data-test, 'continue-shopping')]");
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [Test]
+        public void OrderBackpack()
+        {
+            string username = "standard_user";
+            string password = "secret_sauce";
+            string firstName = "Patrick";
+            string lastName = "Sprague";
+            string zipCode = "97206";
+            IWebElement usernameInput = driver.FindElement(_usernameInputXpath);
+            IWebElement passwordInput = driver.FindElement(_passwordInputXpath);
+            IWebElement loginButton = driver.FindElement(_loginButtonXpath);
+
+            usernameInput.SendKeys(username);
+            passwordInput.SendKeys(password);
+            loginButton.Click();
+            driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
+
+            //add backpack to cart
+            IWebElement addBackpackCart = driver.FindElement(_addBackpackCartXpath);
+            addBackpackCart.Click();
+            //go to shopping cart
+            IWebElement shoppingCart = driver.FindElement(_shoppingCartXpath);
+            shoppingCart.Click();
+            //go to checkout
+            IWebElement checkoutButton = driver.FindElement(_checkoutXpath);
+            checkoutButton.Click();
+            //enter name and zip
+            driver.FindElement(_firstNameXpath).SendKeys(firstName);
+            driver.FindElement(_lastNameXpath).SendKeys(lastName);
+            driver.FindElement(_zipCodeXpath).SendKeys(zipCode);
+            //continue
+            IWebElement continueButton = driver.FindElement(_cartContinueXpath);
+            continueButton.Click();
+            //finish
+            IWebElement finishButton = driver.FindElement(_finishButtonXpath);
+            finishButton.Click();
+            //assert
+            IWebElement successMessage = driver.FindElement(_successMessageXpath);
+            string message = successMessage.Text;
+            Assert.That(message, Does.Contain("Thank you for your order!"));
+        }
+
+        [Test]
+        public void RemoveBackpackFromCart()
+        {
+            string username = "standard_user";
+            string password = "secret_sauce";
+            IWebElement usernameInput = driver.FindElement(_usernameInputXpath);
+            IWebElement passwordInput = driver.FindElement(_passwordInputXpath);
+            IWebElement loginButton = driver.FindElement(_loginButtonXpath);
+
+            usernameInput.SendKeys(username);
+            passwordInput.SendKeys(password);
+            loginButton.Click();
+            driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
+
+            //add backpack to cart
+            IWebElement addBackpackCart = driver.FindElement(_addBackpackCartXpath);
+            addBackpackCart.Click();
+            //go to shopping cart
+            IWebElement shoppingCart = driver.FindElement(_shoppingCartXpath);
+            shoppingCart.Click();
+            //remove backpack from cart
+            IWebElement removeBackpackFromCart = driver.FindElement(_removeBackpackCartXpath);
+            removeBackpackFromCart.Click();
+            //continue shopping
+            IWebElement continueShopping = driver.FindElement(_continueShoppingXpath);
+            continueShopping.Click();
+            //assert
+            Assert.That(driver.FindElement(_addBackpackCartXpath).Displayed, Is.True);
+            
+        }
+
+        [Test]
+        public void FilterProducts()
+        {
+            //navigate to products
+            //filter products
+            //assert
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
